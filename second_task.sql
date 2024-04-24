acdc640232a827daf621d4cdb6a86cbdf3949184
Николай Махов <nmahov2003@gmail.com>
2024-04-24 21:22:22 +0700

    Починил second_task

--- second_task.sql
+++ second_task.sql
@@ -2,24 +2,10 @@
 стоимость покупки не бьётся с её разбивкой по товарам. Выведите такие магазины и дни, в которые в
 них случился сбой, а также сумму расхождения между полной стоимостью покупки и суммой по чеку. */
 
-select s.id, s.name, date_trunc('day', p.datetime)::date as day, sum(p.amount) - (sum(pr.amount_full) - sum(pr.amount_discount)) as div
+select s.name, date_trunc('day', p.datetime)::date as day, p.amount - (sum(pr.amount_full) - sum(pr.amount_discount))
 from shops as s
          join public.employees e on e.shop_id = s.id
          join public.purchases p on e.id = p.seller_id
          join public.purchase_receipts pr on p.id = pr.purchase_id
-group by s.id, s.name, day
-having sum(p.amount) != sum(pr.amount_full) - sum(pr.amount_discount);
-
-
-select s.id, s.name, date_trunc('day', p.datetime)::date as day, sum(p.amount) - (sum(pr.amount_full) - sum(pr.amount_discount)) as div
-from shops as s
-         join public.employees e on e.shop_id = s.id
-         join public.purchases p on e.id = p.seller_id
-         join public.purchase_receipts pr on p.id = pr.purchase_id
-group by s.id, s.name, day;
-
-select *
-from shops as s
-         join public.employees e on e.shop_id = s.id
-         join public.purchases p on e.id = p.seller_id
-         join public.purchase_receipts pr on p.id = pr.purchase_id
+group by s.id, s.name, day, p.id, p.amount
+having  p.amount != sum(pr.amount_full) - sum(pr.amount_discount);
